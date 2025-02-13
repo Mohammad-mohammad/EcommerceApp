6e54d5bd47c684fd1f52e949370a9db8631d5e05
Mohammad Mohammad <it.msm88@gmail.com>
2025-02-13 16:14:47 +0100

    expose entity Ids

--- backend/spring-boot-ecommerce/src/main/java/com/moha/spring_boot_ecommerce/config/MyDataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/moha/spring_boot_ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,30 @@ package com.moha.spring_boot_ecommerce.config;
 
 import com.moha.spring_boot_ecommerce.entity.Product;
 import com.moha.spring_boot_ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import jakarta.persistence.metamodel.Type;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -25,5 +41,21 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metadata, httpMethods)-> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType tempEntityType: entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+//        List<? extends Class<?>> entityClasses = entityManager.getMetamodel().getEntities()
+//                .stream().map(Type::getJavaType).toList();
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+
+        config.exposeIdsFor(domainTypes);
     }
 }
