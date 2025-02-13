a82c09ac59e4918326b206366de354402af01ff1
Mohammad Mohammad <it.msm88@gmail.com>
2025-02-13 15:31:06 +0100

    take care of configure the frontend with categoryId

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -12,7 +12,8 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
+  getProductList(categroyId: number): Observable<Product[]> {
+    
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );  
