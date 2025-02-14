56f8c36f8b7bdc2b4f7968758f1c87b48c4852a4
Mohammad Mohammad <it.msm88@gmail.com>
2025-02-14 13:45:10 +0100

    create product details page

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -8,21 +8,21 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-
+  
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category';
-
+  
   constructor(private httpClient: HttpClient) { }
-
+  
   getProductList(categroyId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categroyId}`;
-  
+    
     return this.getProducts(searchUrl);
   }
   
   searchProducts(theKeyword: string): Observable<Product[]>  {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
-  
+    
     return this.getProducts(searchUrl);
   }
   
@@ -31,13 +31,18 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
-
+  
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );  
   }
-
+  
+  getProduct(theProductId: number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    
+    return this.httpClient.get<Product>(productUrl);
+  }
 }
 
 interface GetResponseProduct {
