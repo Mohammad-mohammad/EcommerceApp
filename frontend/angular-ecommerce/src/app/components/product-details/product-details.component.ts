f09bbc4188bfdaaad91bbe45294f3ddb547e8c24
Mohammad Mohammad <it.msm88@gmail.com>
2025-02-16 15:07:58 +0100

    implement "add to cart" in product details

--- frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -10,9 +12,11 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent implements OnInit {
+
   product!: Product;
 
   constructor(private productService: ProductService, 
+              private cartService: CartService,
               private route: ActivatedRoute) { }
   
   ngOnInit(): void {
@@ -30,5 +34,10 @@ export class ProductDetailsComponent implements OnInit {
     );
   }
 
+  addToChart(product: Product) {
+    console.log(`Adding to cart: ${product.name}, ${product.unitPrice}`);
+    const theCartItem = new CartItem(product);
+    this.cartService.addToCart(theCartItem);
+  }
 
 }
