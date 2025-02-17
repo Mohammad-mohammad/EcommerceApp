1c3fb00034f0f7bbbb43a2d4a1b59d0643d26012
Mohammad Mohammad <it.msm88@gmail.com>
2025-02-17 10:35:58 +0100

    validation for billing address form

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -47,11 +47,11 @@ export class CheckoutComponent implements OnInit {
           zipCode: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
         }),
         billingAddress: this.formBuilder.group({
-          street: [''],
-          city: [''],
-          state: [''],
-          country: [''],
-          zipCode: ['']
+          street: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
+          city: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
+          state: new FormControl('', [Validators.required]),
+          country: new FormControl('', [Validators.required]),
+          zipCode: new FormControl('', [Validators.required, Validators.minLength(2), ShopValidators.notOnlyWhitespace]),
         }),
         creditCard: this.formBuilder.group({
           cardType: [''],
@@ -120,6 +120,17 @@ export class CheckoutComponent implements OnInit {
 
   get shippingAddressZipCode() { return this.checkoutFormGroup.get('shippingAddress.zipCode'); }  
 
+
+  get billingAddressStreet() { return this.checkoutFormGroup.get('billingAddress.street'); }
+  
+  get billingAddressCity() { return this.checkoutFormGroup.get('billingAddress.city'); }
+
+  get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state'); }
+
+  get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
+
+  get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
+  
 
   copyShippingAddressToBillingAddres($event: Event) {
     if(($event.target as HTMLInputElement)?.checked){
